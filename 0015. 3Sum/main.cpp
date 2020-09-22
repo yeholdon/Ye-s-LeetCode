e154aeeab15b84a9b84d5625e662ddbc86d05e80
Honcy Ye <yeholdon@gmail.com>
2020-09-22 08:46:35 +0800

    add 0015 0016

---
+++ 0015. 3Sum/main.cpp
@@ -0,0 +1,11 @@
+#include "solution.h"
+#include "../catch.hpp"
+
+TEST_CASE("3Sum", "[threeSum]")
+{
+    Solution s;
+    vector<int> v1 = { -1,0,1,2,-1,-4 };
+    vector<vector<int>> ans1 = { {-1,-1,2} ,{-1,0,1} };
+    REQUIRE((s.threeSum(v1) == ans1));
+
+}
